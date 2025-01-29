b7578b8cabb3b1f9a345fbf0df41ef97fbb8cf4e
rafaelmeller <rafaelmeller.dev@gmail.com>
2025-01-29 18:45:09 +0530

    Feat: Adding products app

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Native Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Project Apps
+    'products',
 ]
 
 MIDDLEWARE = [
