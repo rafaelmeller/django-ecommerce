ec9b729604ceff20ecbc679dc1b2890a680da652
gabrielayresdev <gabriel.ar.fort@gmail.com>
2025-02-28 07:52:53 -0300

    feat: superadmin based authentication

--- api/views.py
+++ api/views.py
@@ -1,11 +1,15 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from user.models import Item
 from .serializers import ItemSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getData(request):
+    permission_classes = (IsAuthenticated,)
+
     items = Item.objects.all()
     serializer = ItemSerializer(items, many=True)
     return Response(serializer.data)
